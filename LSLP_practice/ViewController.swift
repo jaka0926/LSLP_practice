4d3a4e52721f3fb51b145dfc8de98be9c0c5dffe
Jaka <jaka0926@naver.com>
2024-07-23 15:38:51 +0900

    Initial Commit

---
+++ LSLP_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSLP_practice
+//
+//  Created by Jaka on 2024-07-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
